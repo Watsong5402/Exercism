a950796b3c2108650c82b6bf3fa1644faa3f8363
Graham Watson <grahamwatson@Grahams-Mac-mini.local>
2022-06-22 09:26:48 -0700

    Completed resistor color duo typescript

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,26 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(colors: string[]) {
+  interface ColorDictionary {
+    readonly [color: string]: number;
+  }
+
+  const colorDict: ColorDictionary = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
+  };
+
+  let backwordColors = colors.slice(0, 2).reverse();
+  let sum: number = 0;
+  for (let i = 0; i < backwordColors.length; i++) {
+    let colorValue = colorDict[backwordColors[i]];
+    sum += colorValue * Math.pow(10, i);
+  }
+  return sum;
 }
